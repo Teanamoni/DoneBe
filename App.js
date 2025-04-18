6140b3935edbd940b5c9f2cc4287560eec671f95
Teanamoni <teanamoniimania@gmail.com>
2025-04-18 16:28:53 +0700

    Add splash image and update app.json

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DFF5E1',
     alignItems: 'center',
     justifyContent: 'center',
   },
